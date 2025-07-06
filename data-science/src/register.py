fd096654788eeb88fade2872d39d231f41777db3
cboyd77 <cboyd77@gmail.com>
2025-07-06 12:54:52 -0500

    Update register.py

--- data-science/src/register.py
+++ data-science/src/register.py
@@ -6,29 +6,60 @@ import mlflow
 import pandas as pd
 from pathlib import Path
 
-mlflow.start_run()  # Starting the MLflow experiment run
+def parse_args():
+    '''Parse input arguments'''
 
-def main():
-    # Argument parser setup for command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str, help="Path to the trained model")  # Path to the trained model artifact
-    args = parser.parse_args()
+    parser.add_argument('--model_name', type=str, help='Name under which model will be registered')  # Hint: Specify the type for model_name (str)
+    parser.add_argument('--model_path', type=str, help='Model directory')  # Hint: Specify the type for model_path (str)
+    parser.add_argument("--model_info_output_path", type=str, help="Path to write model info JSON")  # Hint: Specify the type for model_info_output_path (str)
+    args, _ = parser.parse_known_args()
+    print(f'Arguments: {args}')
+
+    return args
+
+
+def main(args):
+    '''Loads the best-trained model from the sweep job and registers it'''
+    print("Registering ", args.model_name)
 
     # Load the trained model from the provided path
-    model = mlflow.sklearn.load_model(args.model)  # 
-
-    print("Registering the best trained used cars price prediction model")
+    model = mlflow.sklearn.load_model(args.model_path)
     
-    # Register the model in the MLflow Model Registry under the name "price_prediction_model"
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        registered_model_name="car_prediction_model",  # Specify the name under which the model will be registered
-        artifact_path="random_forest_price_classifier"  # Specify the path where the model artifacts will be stored
-    )
+    # Log model using mlflow
+     mlflow.sklearn.log_model(model, args.model_name)
 
-    # End the MLflow run
-    mlflow.end_run()  
+    # Register logged model using mlflow
+    run_id = mlflow.active_run().info.run_id
+    model_uri = f'runs:/{run_id}/{args.model_name}'
+    mlflow_model = mlflow.register_model(model_uri, args.model_name)
+    model_version = mlflow_model.version
+
+
+    # Write model info
+    print("Writing JSON")
+    model_info = {"id": f"{args.model_name}:{model_version}"}
+    output_path = os.path.join(args.model_info_output_path, "model_info.json")
+    with open(output_path, "w") as of:
+        json.dump(model_info, of)
 
 if __name__ == "__main__":
-    main()
+    
+    mlflow.start_run()
+    
+    # Parse Arguments
+    args = parse_args()
+    
+    lines = [
+        f"Model name: {args.model_name}",
+        f"Model path: {args.model_path}",
+        f"Model info output path: {args.model_info_output_path}"
+    ]
+
+    for line in lines:
+        print(line)
+
+    main(args)
+
+    mlflow.end_run()
 
